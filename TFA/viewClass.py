028806e118bccdc6a79ae424a302c2bd65b1be03
Luke Fraser <totalvideopro@gmail.com>
2013-05-13 23:10:34 -0700

    added timer

--- TFA/viewClass.py
+++ TFA/viewClass.py
@@ -9,32 +9,75 @@ import math
 class view(QGLWidget):
     def __init__(self):
         super(view, self).__init__()
+        self.time = QTime()
+        self.timer = QTimer()
+        self.x = 0.0
+        self.timer.timeout.connect(self.tick)
+        
 
     def initializeGL(self):
         glEnable(GL_TEXTURE_2D)
         glDisable(GL_DEPTH_TEST)
-        glDisable(GL_COLOR_MATERIAL)
         glEnable(GL_BLEND)
         glEnable(GL_POLYGON_SMOOTH)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         glClearColor(0, 0, 0, 1)
         self.loadImages()
+        self.time.start()
+        self.timer.start(1000/60)
 
     def paintGL(self):
         self.createPlane()
         
     def loadImages(self):
-        image = pygame.image.load("image.JPG")
-        imageGl = pygame.image.tostring(image, "RGB", 1)
 
-        self.textureId = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_2D, self.textureId)
+        # load BackGround Image into Memory
+        imageBG = pygame.image.load("Gameplay.jpg")
+        imageBGGL = pygame.image.tostring(imageBG, "RGB", 1)
+
+        # load Food images into memory
+        foodList = []
+        
+        image = pygame.image.load("apple.png")
+        foodList.append(image)
+
+        image = pygame.image.load("orange.png")
+        foodList.append(image)
+
+        image = pygame.image.load("strawberry.png")
+        foodList.append(image)
+
+        image = pygame.image.load("snickers.png")
+        foodList.append(image)
+
+        image = pygame.image.load("icecreamcone.png")
+        foodList.append(image)
+
+        # load textures to videocard
+        self.textureBGId = glGenTextures(1)
+
+        glBindTexture(GL_TEXTURE_2D, self.textureBGId)
         glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.get_width(), image.get_height(), 0, GL_RGB, GL_UNSIGNED_BYTE, imageGl)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageBG.get_width(), imageBG.get_height(), 0, GL_RGB, GL_UNSIGNED_BYTE, imageBGGL)
+
+        self.textureId = []
+
+        for image in foodList:
+            id = glGenTextures(1)
+            self.textureId.append(id)
+            glImage = pygame.image.tostring(image, "RGBA", 1)
+
+            glBindTexture(GL_TEXTURE_2D, id)
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.get_width(), image.get_height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, glImage)
 
         
 
@@ -42,7 +85,7 @@ class view(QGLWidget):
         glClear(GL_COLOR_BUFFER_BIT)
 
         glEnable( GL_TEXTURE_2D )
-        glBindTexture(GL_TEXTURE_2D, self.textureId)
+        glBindTexture(GL_TEXTURE_2D, self.textureBGId)
 
         glBegin(GL_QUADS)
 
@@ -59,3 +102,31 @@ class view(QGLWidget):
         glVertex2f(1, -1)
 
         glEnd()
+
+        glBindTexture(GL_TEXTURE_2D, self.textureId[0])
+
+        glPushMatrix()
+        glTranslatef(self.x, 0.0, 0.0)
+        glBegin(GL_QUADS)
+
+        glTexCoord2f(0,0)
+        glVertex2f(-.1, -.15)
+
+        glTexCoord2f(0,1)
+        glVertex2f(-.1, .15)
+
+        glTexCoord2f(1,1)
+        glVertex2f(.1, .15)
+
+        glTexCoord2f(1,0)
+        glVertex2f(.1, -.15)
+
+        glEnd()
+        glPopMatrix()
+
+
+    def tick(self):
+        seconds = self.time.restart() * 0.001
+
+        
+        self.update()
